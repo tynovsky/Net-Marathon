1e820626532219f782409d35ef03dc8b61daa2ea
Miroslav Tynovsky <tynovsky@users.noreply.github.com>
2016-12-07 14:07:00 +0100

    initial commit - minilla scaffold

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Net::Marathon - It's new $module
+
+# SYNOPSIS
+
+    use Net::Marathon;
+
+# DESCRIPTION
+
+Net::Marathon is ...
+
+# LICENSE
+
+Copyright (C) Miroslav Tynovsky.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Miroslav Tynovsky <tynovsky@avast.com>
---
+++ lib/Net/Marathon.pm
@@ -0,0 +1,39 @@
+package Net::Marathon;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::Marathon - It's new $module
+
+=head1 SYNOPSIS
+
+    use Net::Marathon;
+
+=head1 DESCRIPTION
+
+Net::Marathon is ...
+
+=head1 LICENSE
+
+Copyright (C) Miroslav Tynovsky.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Miroslav Tynovsky E<lt>tynovsky@avast.comE<gt>
+
+=cut
+
